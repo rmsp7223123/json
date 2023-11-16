abf38214b5158c584d51eb305e96d4b35695ad7d
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-11-16 23:32:19 +0900

    +

---
+++ app/src/main/java/com/example/json/NewsApi.kt
@@ -0,0 +1,28 @@
+package com.example.json
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode:String = "us",
+        @Query("page")
+        pageNumber:Int =1,
+        @Query("apiKey")
+        apiKey:String = "9916ae955df94fbf885d264a385b027f"
+    ): Response<News>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery:String,
+        @Query("page")
+        pageNumber:Int =1,
+        @Query("apiKey")
+        apiKey:String = "9916ae955df94fbf885d264a385b027f"
+    ):Response<News>
+}
